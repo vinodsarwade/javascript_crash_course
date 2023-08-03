39e1bebdc4e89e89e80351ad1902dceddced7bc5
Sarwade <vinod-raghunath.sarwade@capgemini.com>
2023-08-04 00:41:53 +0530

    done

--- main.js
+++ main.js
@@ -27,3 +27,28 @@ console.log(s.toLowerCase());
 console.log(s.split(''));
 
 
+//object literals
+
+const person={
+    firstName:"vinod",
+    lastName:"sarwade",
+    age :23,
+    hobbies:['music','cricket','sports'],
+    address:{
+        street :'hanuman galli',
+        city :'yevti',
+        state:'maharashtra'
+    }
+}
+console.log(person.firstName, person.lastName, person.age);
+console.log(person.hobbies[1]);
+console.log(person.address.city);  
+
+
+const{firstName,lastName,address:{city}}=person;
+console.log(firstName,lastName);
+console.log(city);
+
+
+person.email='vinod@12'
+console.log(person);
--- object_literals.js
+++ object_literals.js
@@ -9,5 +9,4 @@ const person={
         state:'maharashtra'
     }
 }
-
-alert(person);
+console.log(person.firstName);
