2fc05367da2d12f7f9ca540107913b12d8644b84
Sarwade <vinod-raghunath.sarwade@capgemini.com>
2023-08-04 00:02:28 +0530

    changes done by me

---
+++ main.js
@@ -0,0 +1,23 @@
+
+const fruits=['apple','mango'];
+console.log(fruits);
+
+fruits[2]='orange';
+console.log(fruits);
+
+fruits.push('greps');
+fruits.unshift('strwaberry');
+console.log(fruits);
+
+fruits.pop();
+console.log(fruits);
+
+console.log(Array.isArray(fruits));
+
+console.log(fruits.indexOf('mango'));
+
+
+let s='vinod';
+console.log(s.toUpperCase());
+console.log(s.toLowerCase());
+console.log(s.split(''));
